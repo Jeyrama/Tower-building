02c2ada472d2c8a626a31eb0f7882b1d05c3c63a
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-12-07 15:19:54 -0800

    Update Tower-building.js

--- Tower-building.js
+++ Tower-building.js
@@ -35,3 +35,10 @@ function towerBuilder(nFloors) {
 }
 
 // or
+
+function towerBuilder(n) {
+  return Array.from({length: n}, function(v, k) {
+    const spaces = ' '.repeat(n - k - 1);
+    return spaces + '*'.repeat(k + k + 1) + spaces;
+  });
+}
