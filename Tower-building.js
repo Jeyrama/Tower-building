983d361e03879337c3670eb4abce2880898b1e4b
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-12-07 15:19:33 -0800

    Update Tower-building.js

--- Tower-building.js
+++ Tower-building.js
@@ -23,3 +23,13 @@ And a tower with 6 floors looks like this:
 
 
 // Solution
+
+function towerBuilder(nFloors) {
+  let tower = [];
+  for (let i = 0; i < nFloors; i++) {
+    tower.push(" ".repeat(nFloors - i - 1)
+             + "*".repeat((i * 2)+ 1)
+             + " ".repeat(nFloors - i - 1));
+  }
+  return tower;
+}
